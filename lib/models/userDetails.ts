c49b6639177e7c4cd5e546a178f30935b4067f3d
John Farley <john@pcmatic.com>
2025-12-09 20:36:47 -0500

    updated userdetails model

--- lib/models/userDetails.ts
+++ lib/models/userDetails.ts
@@ -1,16 +1,16 @@
 export default interface UserDetails {
     id: string
     userId: string
-	bio: string
+	bio?: string | null
 	chats: string[]
 	comments: string[]
 	createdAt: Date
 	displayName: string
 	followers: string[]
 	following: string[]
-	// avatar: string
+	image?: string | null
 	likedPosts: string[]
 	postIds: string[]
-	suspended: Boolean
+	suspended?: boolean | null
 	updatedAt: Date
 }
