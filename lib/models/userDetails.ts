59509b8271977a6828dddc2bf62f90d5093e3002
John Farley <john@pcmatic.com>
2025-12-09 20:28:47 -0500

    removed avatar for now

--- lib/models/userDetails.ts
+++ lib/models/userDetails.ts
@@ -1,14 +1,14 @@
 export default interface UserDetails {
     id: string
     userId: string
-	bio: string
+	bio: string | null
 	chats: string[]
 	comments: string[]
 	createdAt: Date
 	displayName: string
 	followers: string[]
 	following: string[]
-	// avatar: string
+	avatar: string | null
 	likedPosts: string[]
 	postIds: string[]
 	suspended: Boolean
