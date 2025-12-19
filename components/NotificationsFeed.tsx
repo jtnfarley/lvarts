112a4c6189bfff42f64bc9cc9c89081206e3eb4d
John Farley <john@pcmatic.com>
2025-12-19 18:23:42 -0500

    updated noti styles

--- components/NotificationsFeed.tsx
+++ components/NotificationsFeed.tsx
@@ -71,9 +71,9 @@ export default function NotificationsFeed(props:{user:User, getUser:Function}) {
 				(user && notis && notis.length) &&
                 	notis.map((noti:Notification, index:number) => {
 						return (
-							<div key={index} className={`noti-link flex items-center center-box ${(noti.read) ? 'bg-gray-200' : ''}`}>
+							<div key={index} className={`noti-link flex items-center center-box text-md p-2 ${(noti.read) ? 'bg-gray-200' : ''}`}>
 								<Link href={`/user/${noti.notiUser?.id}`}>
-									<img src={(noti.notiUserDetails?.userDir && noti.notiUserDetails?.avatar) ? `${imageUrl}/${noti.notiUserDetails?.userDir}/${noti.notiUserDetails?.avatar}` : '/images/melty-man.png'} alt={`${noti.notiUserDetails?.displayName} avatar`} className='avatar me-2'/>
+									<img src={(noti.notiUserDetails?.userDir && noti.notiUserDetails?.avatar) ? `${imageUrl}/${noti.notiUserDetails?.userDir}/${noti.notiUserDetails?.avatar}` : '/images/melty-man.png'} alt={`${noti.notiUserDetails?.displayName} avatar`} className='avatar me-2 w-[30px] h-[30px]'/>
 								</Link>
 								<Link href={`/user/${noti.notiUser?.id}`} className='me-1'>
 									{noti.notiUserDetails?.displayName}
