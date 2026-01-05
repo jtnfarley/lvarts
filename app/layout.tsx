de9b6d1e62b5f791613729ee0fc2ea5f21440fab
John Farley <john@pcmatic.com>
2026-01-04 21:30:24 -0500

    switched to nextjs image

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
 	const getRandoImage = ():string => {
 		const randoIndex = Math.floor(Math.random() * bgs.length);
 		const randoVersion = Math.floor(Math.random() * 100000);
-		return `/images/bgs/${bgs[randoIndex]}`;
+		return `/images/bgs/${bgs[randoIndex]}?v=${getRandoInt()}`;
 	}
 
 	const getRandoInt = ():number => {
@@ -46,7 +46,7 @@ export default async function RootLayout({
 					<GoogleAnalytics gaId="G-J6PQBNCBKC" />
 				</head>
 				<body>
-					<div className="relative"><Image src={getRandoImage()} alt='random background image' width={2000} height={1500} key={getRandoInt()} style={{height:'100vh', position:'fixed', width:'100vw', zIndex:-1}}/></div>
+					<div className="relative overflow-hidden"><Image src={getRandoImage()} alt='random background image' width={2000} height={1500} key={getRandoInt()} style={{height:'100vh', position:'fixed', width:'100%', minWidth:'1000px', zIndex:-1}}/></div>
 					
 					<main className='lg:flex lg:justify-center'>
 						<div className="lg:max-w-1300">
