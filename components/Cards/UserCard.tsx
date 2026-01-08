d88b6f2eaacad64d4fd89c4fd739ae3247eb2f3f
John Farley <john@pcmatic.com>
2026-01-07 20:52:25 -0500

    updated usercard styles

--- components/Cards/UserCard.tsx
+++ components/Cards/UserCard.tsx
@@ -29,11 +29,11 @@ const UserCard = ({currentUser, recUserId, displayName, avatar}: Props) => {
 
     return (
         <article className="user-card xl:w-full">
-            <div className="flex items-center xl:bg-orange rounded-full text-white p-2 text-lg gap-3 justify-between">
-                <button className="flex gap-3" onClick={viewProfile} title={displayName}>
+            <div className="flex items-center xl:bg-white rounded-full text-gray-700 p-2 text-sm gap-3 justify-between border-1 border-gray-300 shadow-md">
+                <button className="flex gap-3 items-center" onClick={viewProfile} title={displayName}>
                     <div><img src={avatar} className='rounded-full border-dark-2 w-[50px] h-[50px] xl:w-[35px] xl:h-[35px] xl:border-0'/></div>
                     
-                    <div className='hidden xl:block font-semibold'>{displayName}</div>
+                    <div className='hidden xl:block'>{displayName}</div>
                 </button>
                 <div className='hidden xl:block'>
                     <Follow followUserId={recUserId} user={currentUser}/>
