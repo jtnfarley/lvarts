c5188778a0915dc108d5f00c4b080df82901dec4
John Farley <john@pcmatic.com>
2026-05-26 18:20:35 -0400

    added nav to calendar

--- app/calendar/layout.tsx
+++ app/calendar/layout.tsx
@@ -4,6 +4,7 @@ import TopBar from "@/components/layout/TopBar";
 import LeftSidebar from "@/components/layout/LeftSidebar";
 import BottomBar from "@/components/layout/BottomBar";
 import UserDetails from "@/lib/models/userDetails";
+import Nav from "@/components/layout/Nav";
 
 export const metadata: Metadata = {
   title: "Lehigh Valley Arts & Music",
@@ -32,6 +33,12 @@ export default async function RootLayout({
 			<main className="flex flex-row">
 				<LeftSidebar currentUser={user}/>
 				<section className="calendar-container">
+					{
+						user && 
+						<section className="hidden pt-3 pe-3 xl:flex w-full justify-end">
+							<Nav user={user}/>
+						</section>
+					}
 					<div className="flex flex-col w-full min-h-screen">
 						{children}
 					</div>
